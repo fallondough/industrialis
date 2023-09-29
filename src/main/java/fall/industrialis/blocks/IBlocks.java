0fef97939b9ace5a6f805aa4ec8dc4d7ea6f9162
fallondough <fallondough1@gmail.com>
2023-09-29 21:42:59 +0200

    1.19.3

--- src/main/java/fall/industrialis/blocks/IBlocks.java
+++ src/main/java/fall/industrialis/blocks/IBlocks.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -18,6 +19,9 @@ import java.util.Collection;
 public class IBlocks {
     public static final Collection<ItemStack> BLOCKS = new ArrayList<>();
     public static final Block BAUXITE_ORE = registerBlock("bauxite_ore",
+            new Block(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool()));
+
+    public static final Block ELECTRIC_FURNACE = registerBlock("electric_furnace",
             new Block(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool()));
 
     private static Block registerBlock(String name, Block block) {
