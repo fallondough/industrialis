46a3a3bb304b0d47b0907ed183505b4b40ad8126
fallondough <fallondough1@gmail.com>
2023-10-05 21:42:30 +0200

    1.19.3

--- src/main/java/fall/industrialis/screen/IScreenHandlers.java
+++ src/main/java/fall/industrialis/screen/IScreenHandlers.java
@@ -1,10 +1,6 @@
 package fall.industrialis.screen;
 
-import fall.industrialis.Industrialis;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
 import net.minecraft.screen.ScreenHandlerType;
-import net.minecraft.util.Identifier;
 
 public class IScreenHandlers {
     public static ScreenHandlerType<ElectricFurnaceScreenHandler> ELECTRIC_FURNACE_SCREEN_HANDLER;
